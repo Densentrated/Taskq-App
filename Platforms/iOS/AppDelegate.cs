582ba501a1e5f31d9a8315bae8bb107eb311f6dd
D-108 <72484574+Densentrated@users.noreply.github.com>
2023-06-10 22:11:10 -0700

    Initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Taskq_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
