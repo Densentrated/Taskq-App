24bafcea2dff2b205109f5b6e7bffff6b1d65718
Densentrated <72484574+Densentrated@users.noreply.github.com>
2023-06-11 16:35:02 -0700

    added some code t build the linked lists that are stored

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,3 +15,45 @@ public partial class MainPage : ContentPage
 	}
 }
 
+/**
+namespace LinkedList
+{
+    class Program
+    {
+        public static LinkedList<string> taskList = new LinkedList<String>();
+        public static LinkedList<string> taskTimeList = new LinkedList<String>();
+
+        static void Main()
+        {
+            addTaskAndTime("BrushTeeth and Wash Hair","07:20-08:24");
+            Console.WriteLine(taskList.ElementAt(0));
+            Console.WriteLine(taskList.First);
+        }
+
+        // adds Items to thie following List
+        static void addTaskAndTime(string task, string timeFrame)
+        {
+            taskList.AddLast(task);
+            taskTimeList.AddLast(timeFrame);
+        }
+        
+        // gets the first Five Items From the List
+        static void getTasksAndTimes()
+        {
+
+        }
+
+        // removes a specific Item from the List
+        static void removeTaskandTimes()
+        {
+
+        }
+
+        static void getTaskandTime()
+        {
+
+        }
+    }
+}
+
+**/
