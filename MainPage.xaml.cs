022008078b9becc79bbb82ddd618cb01f4115594
D-108 <72484574+Densentrated@users.noreply.github.com>
2023-07-10 21:09:11 -0700

    started ui overhaul

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Diagnostics;
+using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using ObjectiveLinkedList;
 
@@ -12,62 +14,18 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-
-		//SemanticScreenReader.Announce(CounterBtn.Text);
-	}
-
 	private void OnConfirmTaskButtonClicked(object sender, EventArgs e)
 	{
-		string taskText = taskInput.Text;
-		int taskTime = Int32.Parse(deadlineInput.Text);
-		int taskDurration = Int32.Parse(durrationInput.Text);
+		//string taskText = taskInput.Text;
+		//int taskTime = Int32.Parse(deadlineInput.Text);
+		//int taskDurration = Int32.Parse(durrationInput.Text);
 
-		TasksOfToday.AddObjectiveByTime((taskText, taskTime, taskDurration));
+		//TasksOfToday.AddObjectiveByTime((taskText, taskTime, taskDurration));
 
-	}
-}
+		(string, int, int) task1Data = TasksOfToday.head.data;
+		(string, int, int) task2Data = TasksOfToday.head.next.data;
+		(string, int, int) task3Data = TasksOfToday.head.next.next.data;
 
-/**
-namespace LinkedList
-{
-    class Program
-    {
-        public static LinkedList<string> taskList = new LinkedList<String>();
-        public static LinkedList<string> taskTimeList = new LinkedList<String>();
 
-        static void Main()
-        {
-            addTaskAndTime("BrushTeeth and Wash Hair","07:20-08:24");
-            Console.WriteLine(taskList.ElementAt(0));
-            Console.WriteLine(taskList.First);
-        }
-
-        // adds Items to thie following List
-        static void addTaskAndTime(string task, string timeFrame)
-        {
-            taskList.AddLast(task);
-            taskTimeList.AddLast(timeFrame);
-        }
-        
-        // gets the first Five Items From the List
-        static void getTasksAndTimes()
-        {
-
-        }
-
-        // removes a specific Item from the List
-        static void removeTaskandTimes()
-        {
-
-        }
-
-        static void getTaskandTime()
-        {
-
-        }
     }
 }
-
-**/
--- ObjectiveList.cs
+++ ObjectiveList.cs
@@ -1,132 +1,137 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ObjectiveLinkedList
-{
-        public class Objective
-        {
-        #nullable enable
+namespace ObjectiveLinkedList;
+    public class Objective
+    {
         // defines the data part of the linked list (a tuple )
         // this tuple is in the form of (task, time(24hr clock), duration(in min))
         // and defines the pointer (next)
         public (string, int, int) data;
-            public Objective? next;
+        public Objective? next;
 
-            // constructor
-            public Objective((string, int, int) i)
-            {
-                data = i;
-                next = null;
-            }
-
-
-            // adds an objective to the tail of the list
-            public void AddObjectiveToTail((string, int, int) data)
-            {
-                if (next == null)
-                {
-                    next = new Objective(data);
-                }
-                else
-                {
-                    next.AddObjectiveToTail(data);
-                }
-            }
-
-            public void AddObjectiveByTime((string, int, int) data)
-            {
-                if (next == null)
-                {
-                    next = new Objective(data);
-                }
-                else if (data.Item2 < next.data.Item2)
-                {
-                    Objective temp = new Objective(data);
-                    temp.next = this.next;
-                    this.next = temp;
-                }
-                else
-                {
-                    next.AddObjectiveByTime(data);
-                }
-            }
-
-            // method that prints the current task
-            public void Print()
-            {
-                Console.Write("[" + data + "]->");
-                if (next != null)
-                {
-                    next.Print();
-                }
-            }
-        }
-
-        public class ObjectiveList
+        // constructor
+        public Objective((string, int, int) i)
         {
-            public Objective? head;
+            data = i;
+            next = null;
+        }
 
-            // Constructor for the objective list
-            public ObjectiveList()
+
+        // adds an objective to the tail of the list
+        public void AddObjectiveToTail((string, int, int) data)
+        {
+            if (next == null)
             {
-                head = null;
+                next = new Objective(data);
             }
-
-            // adds an objective to the head of the list
-            public void AddObjectiveToHead((string, int, int) data)
+            else
             {
-                if (head == null)
-                {
-                    head = new Objective(data);
-                }
-                else
-                {
-                    Objective temp = new Objective(data);
-                    temp.next = head;
-                    head = temp;
-                }
-            }
-
-            // adds objective to tail of list
-            public void AddObjectiveToTail((string, int, int) data)
-            {
-                if (head == null)
-                {
-                    head = new Objective(data);
-                }
-                else
-                {
-                    head.AddObjectiveToTail(data);
-                }
-            }
-
-            // adds objective to the list using the time
-            public void AddObjectiveByTime((string, int, int) data)
-            {
-                if (head == null)
-                {
-                    head = new Objective(data);
-                }
-                else if (data.Item2 < head.data.Item2)
-                {
-                    AddObjectiveToHead(data);
-                }
-                else
-                {
-                    head.AddObjectiveByTime(data);
-                }
-            }
-
-            // Prints the Current Objective List
-            public void Print()
-            {
-                if (head != null)
-                {
-                    head.Print();
-                }
+                next.AddObjectiveToTail(data);
             }
         }
-}
+
+        public void AddObjectiveByTime((string, int, int) data)
+        {
+            if (next == null)
+            {
+                next = new Objective(data);
+            }
+            else if (data.Item2 < next.data.Item2)
+            {
+                Objective temp = new Objective(data);
+                temp.next = this.next;
+                this.next = temp;
+            }
+            else
+            {
+                next.AddObjectiveByTime(data);
+            }
+        }
+
+        // method that prints the current task
+        public void Print()
+        {
+            Console.Write("[" + data + "]->");
+            if (next != null)
+            {
+                next.Print();
+            }
+        }
+    }
+
+    public class ObjectiveList
+    {
+        public Objective? head;
+
+        // Constructor for the objective list
+        public ObjectiveList()
+        {
+            head = null;
+        }
+
+        // adds an objective to the head of the list
+        public void AddObjectiveToHead((string, int, int) data)
+        {
+            if (head == null)
+            {
+                head = new Objective(data);
+            }
+            else
+            {
+                Objective temp = new Objective(data);
+                temp.next = head;
+                head = temp;
+            }
+        }
+
+        // removes the objective at the head of the list
+        public void RemoveHead()
+        {
+            if (head != null)
+            {
+                head = head.next;
+            }
+        }
+
+        // adds objective to tail of list
+        public void AddObjectiveToTail((string, int, int) data)
+        {
+            if (head == null)
+            {
+                head = new Objective(data);
+            }
+            else
+            {
+                head.AddObjectiveToTail(data);
+            }
+        }
+
+        // adds objective to the list using the time
+        public void AddObjectiveByTime((string, int, int) data)
+        {
+            if (head == null)
+            {
+                head = new Objective(data);
+            }
+            else if (data.Item2 < head.data.Item2)
+            {
+                AddObjectiveToHead(data);
+            }
+            else
+            {
+                head.AddObjectiveByTime(data);
+            }
+        }
+
+        // Prints the Current Objective List
+        public void Print()
+        {
+            if (head != null)
+            {
+                head.Print();
+            }
+        }
+    }
